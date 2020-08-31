0a19c4f4145f1c25d06fdd6bc3340fce947b6d9b
Taylor Stern <sterntaylor0629@gmail.com>
2020-08-30 17:03:14 -0700

    5.5.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,16 +126,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass(".bottom-trash-drag")
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass(".bottom-trash-drag")
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(this).addClass("dropover-active")
+    $(".bottom-trash").addClass(".bottom-trash-active")
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(this).removeClass("dropover-active")
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   },
   update: function (event) {
     // array to store the task data in
@@ -201,6 +205,7 @@ var auditTask = function (taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl)
 };
 
 // modal was triggered
@@ -216,7 +221,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -248,5 +253,8 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
-
-
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
